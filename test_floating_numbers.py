6212d18352ea6005b66c50af7fd03b64ac54ded9
Andre Massing <andre.massing@ntnu.no>
2025-01-24 15:52:50 +0100

    Added missing files for composite qr implementation

---
+++ test_floating_numbers.py
@@ -0,0 +1,17 @@
+import pytest
+
+def calculate_sum(a, b):
+    return a + b
+
+def test_calculate_sum():
+    a = 0.2
+    b = 0.2
+    sum = 0.4
+    assert a + b == sum
+    # assert a + b == pytest.approx(sum)
+    
+
+@pytest.mark.parametrize("a, b, sum", [(0.1, 0.1, 0.2), (0.2, 0.2, 0.4), (0.4, 0.4, 0.8)])
+def test_calculate_sum_parametrized(a, b, sum):
+    assert a + b == sum
+    # assert a + b == pytest.approx(sum)
